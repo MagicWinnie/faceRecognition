57c7d552ec7e3de7e273209be60fba2eeb731a52
Dmitrii <39788810+MagicWinnie@users.noreply.github.com>
2019-05-16 10:37:22 +0700

    xfngd

--- fr_example2.py
+++ fr_example2.py
@@ -3,6 +3,10 @@ import cv2
 import numpy as np
 import pickle
 import logging
+face_locations = []
+face_encodings = []
+face_names = []
+prev_names = []
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 video_capture = cv2.VideoCapture(0)
 print("Turned ON Camera")
@@ -26,76 +30,31 @@ while True:
     ret, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    
+    rgb_frame = frame[:, :, ::-1]
     if len(faces)>0:
-        rgb_frame = frame[:, :, ::-1]
         if count%2==0:
-            # Find all the faces and face enqcodings in the frame of video
             face_locations = face_recognition.face_locations(rgb_frame)
             face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-
-            # Loop through each face in this frame of video
-            for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            face_names = []
+            for face_encoding in face_encodings:
                 # See if the face is a match for the known face(s)
                 matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
                 name = "Unknown"
-
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
-                
-                if len(names)<5:
-                    names.append(name)
-                else:
-                    names = names[:4:]
-                    for i in student:
-                        if i in names and aas!=1:
-                            logging.debug(i + ' student in the frame')
-                            aas=1
-                        else:
-                            if aas==1:
-                                logging.debug(i + ' student disappeared')
-                                aas=0
-                            else:
-                                logging.debug('unknown')
-                    for j in teacher:
-                        if j in names and aat!=1:
-                            logging.debug(j + ' teacher in the frame')
-                            aat=1
-                        else:
-                            if aat==1:
-                                logging.debug(j + ' teacher disappeared')
-                                aat=0
-                            else:
-                                logging.debug('unknown')
-                    names.clear()
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                prev = name
-
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-        else:
-            for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
-                name = "Unknown"
-
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
                 best_match_index = np.argmin(face_distances)
                 if matches[best_match_index]:
                     name = known_face_names[best_match_index]
 
-                # Draw a box around the face
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                face_names.append(name)
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+        
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
-                # Draw a label with a name below the face
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)    
     count +=1
     # Display the resulting image
     cv2.imshow('Video', frame)
